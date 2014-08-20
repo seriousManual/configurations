61d962b435162abd1e090e30522e3966e992cd74
zaphod1984 <zaphod84@gmx.de>
2014-08-20 08:14:54 +0200

    switches ce with hoek

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 
-var ce = require('cloneextend');
+var hoek = require('hoek');
 
 var KEY_EXTERNAL_FILE = 'externalconfig';
 var KEY_DEVELOPMENT = 'development';
@@ -28,13 +28,13 @@ function load(baseDir, cliArgs) {
 
     //checking if there's a config file named after the current environment. file name pattern: {environmentName}.js
     if (fs.existsSync(pathEnvironment)) {
-        configuration = ce.extend(configuration, require(pathEnvironment));
+        configuration = hoek.merge(configuration, require(pathEnvironment));
     }
 
     //we're checking if there's a config file named after the current user, but only in development. file name pattern: user.{userName}.js
     if (environment === KEY_DEVELOPMENT) {
         if (fs.existsSync(pathUser)) {
-            configuration = ce.extend(configuration, require(pathUser));
+            configuration = hoek.merge(configuration, require(pathUser));
         }
     }
 
@@ -43,7 +43,7 @@ function load(baseDir, cliArgs) {
         var externalConfigPath = cliArgs[KEY_EXTERNAL_FILE];
 
         if (fs.existsSync(externalConfigPath)) {
-            configuration = ce.extend(configuration, require(externalConfigPath));
+            configuration = hoek.merge(configuration, require(externalConfigPath));
         } else {
             throw new Error('supplied external config file could not be found');
         }
@@ -52,10 +52,10 @@ function load(baseDir, cliArgs) {
     }
 
     if (Object.keys(cliArgs).length > 0) {
-        ce.extend(configuration, cliArgs);
+        hoek.merge(configuration, cliArgs);
     }
 
-    return ce.clone(configuration);
+    return hoek.clone(configuration);
 }
 
 module.exports.load = load;
